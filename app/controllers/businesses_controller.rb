09db2be78213573ca0fdd229abaf928f5e947212
Nick Ramsey <nramseysc@gmail.com>
2019-08-23 13:57:06 -0700

    add business controller

---
+++ app/controllers/businesses_controller.rb
@@ -0,0 +1,43 @@
+class BusinessesController < ApplicationController
+
+  def index
+    @businesses = Business.all
+    render json: @businesses
+  end
+
+  def show
+    render json: @business
+  end
+
+  def create
+    @business = Business.new(business_params)
+
+    if @business.save
+      render json: @business, status: :created, location: @business
+    else
+      render json: @business.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @business.update(business_params)
+      render json: @business
+    else
+      render json: @business.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @business.destroy
+  end
+
+  private
+
+  def set_business
+    @business = Business.find(params[:id])
+  end
+
+  def business_params
+    params.require(:business).permit(:name, :description, :location)
+  end
+end
